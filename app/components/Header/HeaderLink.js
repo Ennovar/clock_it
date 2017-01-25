19af98d3b4994a517ce1b453869d333dea934b9d
Austin Crane <austin.crane@ennovar.wichita.edu>
2017-01-25 14:14:37 -0600

    Made some changes to the App header and inputs (#1)

--- app/components/Header/HeaderLink.js
+++ app/components/Header/HeaderLink.js
@@ -2,24 +2,32 @@ import { Link } from 'react-router';
 import styled from 'styled-components';
 
 export default styled(Link)`
-  display: inline-flex;
-  padding: 0.25em 2em;
-  margin: 1em;
-  text-decoration: none;
+  width: 200px;
+  border: 2px solid #9a9a97;
   border-radius: 4px;
+  color: #9a9a97;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 1em;
+  padding: 0.25em 2em;
+  text-align: center;
+  text-decoration: none;
   -webkit-font-smoothing: antialiased;
   -webkit-touch-callout: none;
   user-select: none;
-  cursor: pointer;
-  outline: 0;
-  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  font-weight: bold;
-  font-size: 16px;
-  border: 2px solid #9a9a97;
-  color: #9a9a97;
+  outline: none;
 
-  &:active {
+
+  &:active,
+  &:hover {
     background: #9a9a97;
     color: #FFF;
+  }
+
+  span {
+    margin: 0 auto;
   }
 `;
